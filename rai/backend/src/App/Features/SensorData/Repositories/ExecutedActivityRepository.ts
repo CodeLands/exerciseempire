fb99a8ea8f2569ab8a013be01643d9a174ca88df
Grega Zorko <zorko.grega@gmail.com>
2024-05-22 20:41:12 +0200

    Defined routes: POST: /create-executed-activity, POST /toggle-activity, and logic to create new instance of activiy that is being tracked and ability to toggle start / stop on is active

--- rai/backend/src/App/Features/SensorData/Repositories/ExecutedActivityRepository.ts
+++ rai/backend/src/App/Features/SensorData/Repositories/ExecutedActivityRepository.ts
@@ -13,17 +13,52 @@ export class ExecutedActivityRepository {
   @inject(TYPES.DbGateway)
   private readonly dbGateway: DbGateway = null!;
 
-  public async getById( executed_activity_id: number ): Promise<RepositoryResult<z.infer<typeof ExecutedActivitiySchema>>> {
-    const result = await this.dbGateway.query(
-        "SELECT * FROM ExecutedActivities WHERE id = $1",
-        [executed_activity_id],
-      );
+  public async create( activity_id: number, user_id: number ): Promise<RepositoryResult<z.infer<typeof ExecutedActivitiySchema>>> {
+    const result = await this.dbGateway.query(`
+        INSERT INTO ExecutedActivities (activity_id, user_id, is_active, start_time, duration)
+        VALUES (${activity_id}, ${user_id}, false, NOW(), 0)`)
+
     if (!result.dbSuccess)
         return {
             status: RepositoryResultStatus.dbError,
             errors: ["Database error!"],
         };
 
+    if (result.data.length === 0)
+        return {
+            status: RepositoryResultStatus.failed,
+            messages: ["ExecutedActivity not created!"],
+        };
+
+    const executedActivityData = result.data[0];
+    const validationResult = ExecutedActivitiySchema.safeParse(executedActivityData);
+
+    if (!validationResult.success)
+        return {
+            status: RepositoryResultStatus.zodError,
+            errors: [
+                "Invalid data from database!",
+                ...validationResult.error.errors.map((e) => e.message),
+            ],
+        };
+
+    return {
+        status: RepositoryResultStatus.success,
+        data: validationResult.data,
+    }
+  }
+
+  public async getById( executed_activity_id: number ): Promise<RepositoryResult<z.infer<typeof ExecutedActivitiySchema>>> {
+    const result = await this.dbGateway.query(
+        "SELECT * FROM ExecutedActivities WHERE id = $1",
+        [executed_activity_id],
+      );
+
+    if (!result.dbSuccess)
+        return {
+            status: RepositoryResultStatus.dbError,
+            errors: ["Database error!"],
+        };
 
     if (result.data.length === 0)
         return {
@@ -31,17 +66,86 @@ export class ExecutedActivityRepository {
             messages: ["ExecutedActivity not found!"],
         };
 
-
-    const newSensorData = result.data[0];
-    const validationResult = ExecutedActivitiySchema.safeParse(newSensorData);
+    const executedActivityData = result.data[0];
+    const validationResult = ExecutedActivitiySchema.safeParse(executedActivityData);
 
     if (!validationResult.success)
         return {
           status: RepositoryResultStatus.zodError,
           errors: [
             "Invalid data from database!",
-            validationResult.error.flatten().fieldErrors.toString(),
+            ...validationResult.error.errors.map((e) => e.message),
           ],
+        };
+
+    return {
+        status: RepositoryResultStatus.success,
+        data: validationResult.data,
+    }
+  }
+
+  public async getByActivityId( activity_id: number ): Promise<RepositoryResult<z.infer<typeof ExecutedActivitiySchema>[]>> {
+    const result = await this.dbGateway.query(
+        "SELECT * FROM ExecutedActivities WHERE activity_id = $1",
+        [activity_id],
+      );
+    if (!result.dbSuccess)
+        return {
+            status: RepositoryResultStatus.dbError,
+            errors: ["Database error!"],
+        };
+
+    if (result.data.length === 0)
+        return {
+            status: RepositoryResultStatus.failed,
+            messages: ["Not executed activities found of this activity type!"],
+        };
+
+    const validationResult = z.array(ExecutedActivitiySchema).safeParse(result.data);
+
+    if (!validationResult.success)
+        return {
+            status: RepositoryResultStatus.zodError,
+            errors: [
+                "Invalid data from database!",
+                ...validationResult.error.errors.map((e) => e.message),
+            ],
+        };
+
+    return {
+        status: RepositoryResultStatus.success,
+        data: validationResult.data,
+    }
+  }
+
+  public async toggleIsActive( executed_activity_id: number, is_active: boolean ): Promise<RepositoryResult<z.infer<typeof ExecutedActivitiySchema>>> {
+    const result = await this.dbGateway.query(
+        "UPDATE ExecutedActivities SET is_active = $1 WHERE id = $2 RETURNING *",
+        [is_active, executed_activity_id],
+      );
+
+    if (!result.dbSuccess)
+        return {
+            status: RepositoryResultStatus.dbError,
+            errors: ["Database error!"],
+        };
+
+    if (result.data.length === 0)
+        return {
+            status: RepositoryResultStatus.failed,
+            messages: ["ExecutedActivity not found!"],
+        };
+
+    const executedActivityData = result.data[0];
+    const validationResult = ExecutedActivitiySchema.safeParse(executedActivityData);
+
+    if (!validationResult.success)
+        return {
+            status: RepositoryResultStatus.zodError,
+            errors: [
+                "Invalid data from database!",
+                ...validationResult.error.errors.map((e) => e.message),
+            ],
         };
 
     return {
--- rai/backend/src/App/Features/SensorData/SensorDataValidator.ts
+++ rai/backend/src/App/Features/SensorData/SensorDataValidator.ts
@@ -2,7 +2,16 @@ import { injectable } from 'inversify';
 import z from 'zod';
 import { ValidationResult } from '/App/Types/ValidatorTypes';
 
-export type SensorData = {
+export type CreateActivityPayload = {
+    activity_id: number;
+    user_id: number;
+};
+
+export type ToggleActivityPayload = {
+    executed_activity_id: number;
+}
+
+export type SensorDataPayload = {
     executed_activity_id: number;
     sensor_id: number;
     value: number;
@@ -11,7 +20,48 @@ export type SensorData = {
 
 @injectable()
 export class SensorDataValidator {
-    public sensorDataValidate = (executed_activity_id: number, sensor_id: number, value: number, timestamp: number): ValidationResult<SensorData> => {
+    public createActivityValidate = (executed_activity_id: number, user_id: number): ValidationResult<CreateActivityPayload> => {
+        const schema = z.object({
+            activity_id: z.number({
+                required_error: "executed_activity_id is required"
+            }).nonnegative({
+                message: "executed_activity_id is required and must be nonnegative"
+            }),
+            user_id: z.number({
+                required_error: "user_id is required"
+            }).nonnegative({
+                message: "user_id is required and must be nonnegative"
+            })
+        });
+    
+        const validationResult = schema.safeParse({ executed_activity_id, user_id });
+    
+        if (!validationResult.success) {
+            return { success: false, errors: validationResult.error.errors };
+        } else {
+            return { success: true, data: validationResult.data };
+        }
+    }
+
+    public toggleActivityValidate = (activity_id: number): ValidationResult<ToggleActivityPayload> => {
+        const schema = z.object({
+            executed_activity_id: z.number({
+                required_error: "activity_id is required"
+            }).nonnegative({
+                message: "activity_id is required and must be nonnegative"
+            })
+        });
+    
+        const validationResult = schema.safeParse({ activity_id });
+    
+        if (!validationResult.success) {
+            return { success: false, errors: validationResult.error.errors };
+        } else {
+            return { success: true, data: validationResult.data };
+        }
+    }
+
+    public sensorDataValidate = (executed_activity_id: number, sensor_id: number, value: number, timestamp: number): ValidationResult<SensorDataPayload> => {
         const schema = z.object({
             executed_activity_id: z.number({
                 required_error: "executed_activity_id is required"
